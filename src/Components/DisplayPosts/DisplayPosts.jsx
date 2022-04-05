fd87499c7b9215bd878e8bfc08e8997c41d04a6a
joecoxstrong <joecoxstrong@gmail.com>
2022-04-05 09:40:34 -0400

    Added boxes to DisplayPosts so that each post will fall inside of its own box to separate it from other posts

--- src/Components/DisplayPosts/DisplayPosts.jsx
+++ src/Components/DisplayPosts/DisplayPosts.jsx
@@ -5,11 +5,12 @@ const DisplayPosts = (props) => {
         
         
           
-                <div class="media-body">
+                <div class="media-body" >
                     {props.parentEntries.map((entry)=>{
                         return (
+                            <div className="border-box">
                     <><h4 class="media-heading">{entry.name}</h4><div>{entry.post}</div></>
-                
+                            </div>
                 // <tr>
                 //     <td>{entry.name}</td>
                 //     <td>{entry.post}</td>
